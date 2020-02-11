d06cfc0085ff5630f525b51fe3e385387de9abcc
romanKadoutchek <41930387+romanKadoutchek@users.noreply.github.com>
2020-02-11 12:29:21 -0500

    Update Ruler.cs

--- Scripts/Ruler.cs
+++ Scripts/Ruler.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Ruler : MonoBehaviour { 
 
-	private float RScale = 1.0;
+	//private float RScale = 1.0;
 //should it be in the corner, or should the user be able to move it to measure specific areas?
 
 /*uses fov to determine scale
