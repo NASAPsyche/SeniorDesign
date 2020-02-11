b3f9ff1d945a8a7c9babf22416f6ca836a10d602
NateUllman <60152037+NateUllman@users.noreply.github.com>
2020-02-10 22:18:33 -0500

    Add files via upload

---
+++ Scripts/Ruler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ruler : MonoBehaviour { 
+
+	private float RScale = 1.0;
+//should it be in the corner, or should the user be able to move it to measure specific areas?
+
+/*uses fov to determine scale
+	void Update()
+    {
+		RScale = GetComponent<Camera>().fieldOfView;
+	}
+*/
+
+}
