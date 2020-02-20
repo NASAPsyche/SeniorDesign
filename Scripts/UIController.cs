bb12e804d47f06573d1634640eb6433bff3ef594
NateUllman <60152037+NateUllman@users.noreply.github.com>
2020-02-19 22:30:32 -0500

    Add files via upload
    The ruler isn't finished, but it is close. This script can be used to house all of the UI canvas elements.

---
+++ Scripts/UIController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+	//Set up a game object for the canvas and various UI elements
+	public GameObject MainUI_Canvas = new GameObject();
+	GameObject RulerUI = new GameObject();
+	GameObject HideRuler = new GameObject();
+
+	//Make the UI elements a child of the canvas
+	RulerUI.transform.SetParent(MainUI_Canvas.transform, false);
+
+	//I'm not sure if the UI components need to be initialized in start or not
+	void Start(){
+		//Initialize Ruler components
+		Image rulerImg = RulerUI.AddComponent<Image>();
+		Text leftzero = RulerUI.AddComponent<Text>(); //go ahead and change the name if you wish
+		Text dynamicMeasure = RulerUI.AddComponent<Text>();
+
+		//set the image's albedo color to black so that it can turn trasnparent
+		var temp = rulerImg.color;
+		temp.a = 0.0f;
+		rulerImg.color = temp;
+	}
+}
