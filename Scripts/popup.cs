6988eda6136e629285afebde468880f1721b9478
creaser16 <34587574+creaser16@users.noreply.github.com>
2020-02-24 19:02:26 -0500

    AsteroidI Update
    Proof of Concept: When a point on the asteroid is clicked (marked by white box InfoPoint) a message to the console is logged with the information. Is there anyway to have the console logs show up in the unity web?

---
+++ Scripts/popup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        // this object was clicked - do something
+        print("This point on the asteroid is really cool! It is a point on the asteroid. How neat.");
+    }
+}
