86e6e49e78fb211fb615b72704024b634fbf510c
romanKadoutchek <41930387+romanKadoutchek@users.noreply.github.com>
2020-04-06 09:25:06 -0400

    Update CameraZoom.cs
    This makes it so the background stays stationary when users are zooming in/out.
    
    This moves the camera position instead of adjusting the Field of View for zooming.

--- Scripts/CameraZoom.cs
+++ Scripts/CameraZoom.cs
@@ -2,22 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Created FPSController [Standard Assets > Characters]
 public class CameraZoom : MonoBehaviour
 {
-    private int zoomIn = 5;
-    private int zoomOut = 5;
+    private float X = 60f;
+    private float Y = 60f;
+    private float Z = 60f;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
-            GetComponent<Camera>().fieldOfView -= zoomIn;
+            X -= 2f;
+            Y -= 2f;
+            Z -= 2f;
+            GetComponent<Camera>().transform.position = new Vector3(X, Y, Z);
         }
         if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
-            GetComponent<Camera>().fieldOfView += zoomOut;
+            X += 2f;
+            Y += 2f;
+            Z += 2f;
+            GetComponent<Camera>().transform.position = new Vector3(X, Y, Z);
         }
     }
 }
