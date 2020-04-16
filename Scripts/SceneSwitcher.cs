c5bb1c55f3c1ac7d46cbe7144ef0cf5a74b25d89
romanKadoutchek <41930387+romanKadoutchek@users.noreply.github.com>
2020-04-15 21:41:33 -0400

    Update SceneSwitcher.cs
    Added a new information scene.

--- Scripts/SceneSwitcher.cs
+++ Scripts/SceneSwitcher.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class SceneSwitcher : MonoBehaviour
 {
     public void NextScene()
     {
         SceneManager.LoadScene("Asteroid");
+    }
+    public void InfoScene()
+    {
+        SceneManager.LoadScene("InfoScene");
     }
 }
